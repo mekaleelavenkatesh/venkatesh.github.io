5dd1da88ef124bb8d140bc4ab881d6d16f781e47
Meka Leela Venkatesh <mekaleelavenkatesh@gmail.com>
2022-12-27 20:53:55 +0530

    Update counter.js

--- counter.js
+++ counter.js
@@ -6,12 +6,13 @@ function updateCounter() {
      * if count is null then set count to 1
     * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
     */
-   if (count == null){
-          count = 1;
+    if (count == String(null)){
+       count = 1;
    }
    else{
-          count += 1;
+       count=parseInt(count)+1;
    }
+       
     localStorage.setItem('visiterCount', count);
     counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterCount')}`;
 }
